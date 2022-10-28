4a687bc4199af6ab2e5b7aa30ae8a6603b684832
Turan98 <48137040+Turan98@users.noreply.github.com>
2022-10-28 12:48:42 +0400

    test9

---
+++ okruqleniye/Program9.cs
@@ -0,0 +1,10 @@
+﻿// rounding
+decimal value ;
+int n;
+Console.WriteLine("enter num which you want to rounding: ");
+value = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("write approximation: ");
+n = Convert.ToInt32(Console.ReadLine());
+decimal math = decimal.Round(value, n, MidpointRounding.ToEven);
+Console.WriteLine("your arrounding number: " + math);
+
